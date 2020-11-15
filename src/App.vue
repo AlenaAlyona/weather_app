c9be64583de4b937256015b81c3904a8a576da91
AlenaAlyona <izakson91@gmail.com>
2020-11-15 21:09:43 +0100

    added country code select

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,39 @@
         <img alt="cloud" class="cloud" src="./assets/cloud.png" />
         <img alt="sun" class="sun" src="./assets/sun.png" />
       </div>
+      <div>
+        <v-select class="selected" label="alpha2Code" :options="countries">
+          <template slot="option" slot-scope="option">
+            <img :src="option.flag" />
+            {{ option.alpha2Code }}
+          </template>
+          <template v-slot:selected-option="option">
+            <img :src="option.flag" />
+            {{ option.alpha2Code }}
+          </template>
+        </v-select>
+      </div>
+      <div class="select-country"></div>
     </div>
   </div>
 </template>
 
 <script>
-const API_KEY = process.env.VUE_APP_APIKEY;
+import axios from "axios";
+// const API_KEY = process.env.VUE_APP_APIKEY;
+
 export default {
   name: "App",
+  data() {
+    return {
+      countries: [],
+      selectedCountry: null,
+    };
+  },
   mounted() {
-    console.log("API KEY", API_KEY);
+    axios
+      .get("https://restcountries.eu/rest/v2/all?fields=alpha2Code;flag")
+      .then((response) => (this.countries = response.data));
   },
 };
 </script>
@@ -34,6 +57,7 @@ export default {
 
 .search-box {
   position: absolute;
+  display: flex;
   width: 632px;
   height: 92px;
   left: 404px;
@@ -50,15 +74,14 @@ export default {
 }
 
 .image-box {
-  position: fixed;
   width: 48px;
   height: 48px;
   left: 426px;
   top: 215px;
+  background-color: aqua;
 }
 
 .cloud {
-  position: fixed;
   width: 40px;
   height: 22.35px;
   left: 432px;
@@ -69,12 +92,65 @@ export default {
 }
 
 .sun {
-  position: fixed;
   width: 20.58px;
   height: 18px;
   left: 428px;
   top: 224px;
 
   color: #ffd600;
+}
+
+img {
+  height: auto;
+  max-width: 2.5rem;
+  /* margin-right: 1rem; */
+}
+
+.d-center {
+  display: flex;
+  align-items: center;
+}
+
+.selected img {
+  width: auto;
+  max-height: 23px;
+  margin-right: 0.5rem;
+}
+
+.v-select .dropdown li {
+  border-bottom: 1px solid rgba(112, 128, 144, 0.1);
+  width: 120px;
+}
+
+.v-select .dropdown li:last-child {
+  border-bottom: none;
+  width: 120px;
+}
+
+.v-select .dropdown li a {
+  padding: 10px 20px;
+  /* width: 100%; */
+  width: 120px;
+  font-size: 1.25em;
+  color: #3c3c3c;
+}
+
+.v-select .dropdown-menu .active > a {
+  color: #fff;
+}
+
+/* .selected .vs__dropdown-menu {
+  width: 120px;
+  height: 48px;
+  left: 486px;
+  top: 396px;
+} */
+.selected .vs__search::placeholder,
+.selected .vs__dropdown-toggle,
+.selected .vs__dropdown-menu {
+  border: none;
+  text-transform: lowercase;
+  font-variant: small-caps;
+  width: 95px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from "vue";
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import App from "./App.vue";
 
+Vue.use(VueAxios, axios);
+
 Vue.config.productionTip = false;
+
+Vue.component("v-select", vSelect);
 
 new Vue({
   render: (h) => h(App),
