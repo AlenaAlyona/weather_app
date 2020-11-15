6cd574a3c56da299644d7587cdba210f6a19783d
AlenaAlyona <izakson91@gmail.com>
2020-11-15 15:33:15 +0100

    added image to search box

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <div class="search-box"></div>
+    <div class="search-box">
+      <div class="image-box">
+        <img alt="cloud" class="cloud" src="./assets/cloud.png" />
+        <img alt="sun" class="sun" src="./assets/sun.png" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -42,5 +47,34 @@ export default {
     #f8f8f8;
   box-shadow: 0px 2px 10px rgba(8, 21, 62, 0.15);
   border-radius: 16px;
+}
+
+.image-box {
+  position: fixed;
+  width: 48px;
+  height: 48px;
+  left: 426px;
+  top: 215px;
+}
+
+.cloud {
+  position: fixed;
+  width: 40px;
+  height: 22.35px;
+  left: 432px;
+  top: 232px;
+
+  color: #b5c7ff;
+  opacity: 0.5;
+}
+
+.sun {
+  position: fixed;
+  width: 20.58px;
+  height: 18px;
+  left: 428px;
+  top: 224px;
+
+  color: #ffd600;
 }
 </style>
